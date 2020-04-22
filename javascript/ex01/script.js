ad4fb8314025ca74f7911e212a411c5f32cea994
JJangseong <sungjin5891@gmail.com>
2020-04-22 21:20:44 +0900

    JAVASCRIPT - FaceBoox 02

--- javascript/ex01/script.js
+++ javascript/ex01/script.js
@@ -1,11 +1,19 @@
-var database = [
+let database = [
   {
     username: "admin",
     password: "1",
   },
+  {
+    username: "admin1",
+    password: "1",
+  },
+  {
+    username: "admin2",
+    password: "1",
+  },
 ];
 
-var newsFeed = [
+let newsFeed = [
   {
     username: "Bobby",
     timeline: "So tired from all that learning!",
@@ -16,14 +24,22 @@ var newsFeed = [
   },
 ];
 
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's tour password?");
+const userNamePrompt = prompt("What's your username?");
+const passwordPrompt = prompt("What's tour password?");
 
-function singIn(user, pass) {
-  if (user === database[0].username && pass === database[0].password) {
+function isUserValid(username, password) {
+  const result = database.filter(
+    (user) => user.username === username && user.password === password
+  );
+  console.log({ result });
+  return result.length > 0 ? true : false;
+}
+
+function singIn(username, password) {
+  if (isUserValid(username, password)) {
     console.log(newsFeed);
   } else {
-    alert("Sorry, wrong username and password!");
+    alert("Sorry, wrong username and password");
   }
 }
 
