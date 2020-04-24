a7605911180b89f0802b30cd417fdde1fa5fb22e
JJangseong <sungjin5891@gmail.com>
2020-04-24 22:50:55 +0900

    REACT - RobotList

---
+++ react/ex01/src/CardList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((robot) => {
+        return (
+          <Card
+            key={robot.id}
+            name={robot.name}
+            username={robot.username}
+            email={robot.email}
+            id={robot.id}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
--- react/ex01/src/index.js
+++ react/ex01/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CardList from "./CardList";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { robots } from "./robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CardList robots={robots}/>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
