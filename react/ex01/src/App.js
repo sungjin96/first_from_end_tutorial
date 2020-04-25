e045536a88613a55c74779ca31e799b95d0f94b0
JJangseong <sungjin5891@gmail.com>
2020-04-25 13:54:25 +0900

    REACT - LifeCycle

--- react/ex01/src/App.js
+++ react/ex01/src/App.js
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 const App = () => {
   const [search, setSearch] = useState("");
-  const [robotList, setRobotList] = useState(robots);
+  const [robotList, setRobotList] = useState([]);
 
   const onChange = (e) => {
     setSearch(e.target.value);
   };
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((users) => {
+        console.log(users);
+        setRobotList(users);
+      });
+  }, []);
+
   const filteredRobots = robotList.filter((robot) => {
     return robot.name.toLowerCase().includes(search.toLowerCase());
   });
+
+  if (robotList.length === 0) return <h1>Loading.....</h1>;
 
   return (
     <div className="tc">
