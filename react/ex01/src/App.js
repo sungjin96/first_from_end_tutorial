d1ed06957326f45cfa24b6e096ccb0704b21099a
JJangseong <sungjin5891@gmail.com>
2020-04-25 12:54:58 +0900

    REACT - Robot Background update

--- react/ex01/src/App.js
+++ react/ex01/src/App.js
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import CardList from "./CardList";
-import SearchBox from './SearchBox';
+import SearchBox from "./SearchBox";
 import { robots } from "./robots";
+import "./App.css";
 
 const App = () => {
-
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [robotList, setRobotList] = useState(robots);
 
   const onChange = (e) => {
     setSearch(e.target.value);
-  }
+  };
 
-  const filteredRobots = robotList.filter(robot => {
-    return robot.name.toLowerCase().includes(search.toLowerCase())
-  })
+  const filteredRobots = robotList.filter((robot) => {
+    return robot.name.toLowerCase().includes(search.toLowerCase());
+  });
 
   return (
     <div className="tc">
-      <h1>RobotFriends</h1>
-      <SearchBox onChange={onChange}/>
-      <CardList robots={filteredRobots}/>
+      <h1 className="f1">RobotFriends</h1>
+      <SearchBox onChange={onChange} />
+      <CardList robots={filteredRobots} />
     </div>
   );
 };
