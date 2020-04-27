9adbd3df77daefa959fd525ebd7754e2209d70ae
JJangseong <sungjin5891@gmail.com>
2020-04-27 22:18:53 +0900

    REACT - 얼굴 인식 API

--- face_brain/src/App.js
+++ face_brain/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: "b7c8c83c9e224468a02bb1c56dc7b8bc",
+});
 
 const particlesOptions = {
   particles: {
@@ -19,18 +25,38 @@ const particlesOptions = {
 };
 
 function App() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        input
+      )
+      .then(
+        function (res) {
+          console.log(res.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function (err) {}
+      );
+  };
   return (
     <div className="App">
       <Particles params={particlesOptions} className="particles" />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {
-        //
-        //
-        // <FaceRecognition />
-      }
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
+      <FaceRecognition imageUrl={imageUrl} />
     </div>
   );
 }
