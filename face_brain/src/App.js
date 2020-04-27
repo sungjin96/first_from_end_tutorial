f259af915ba646a76f0b8032b4abff2c6e3cd1ce
JJangseong <sungjin5891@gmail.com>
2020-04-27 21:38:38 +0900

    REACT - Brain UI

---
+++ face_brain/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Particles from "react-particles-js";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Particles params={particlesOptions} className="particles" />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {
+        //
+        //
+        // <FaceRecognition />
+      }
+    </div>
+  );
+}
+
+export default App;
