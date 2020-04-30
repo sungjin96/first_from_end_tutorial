bee8914b6115dda2fe039e9b0361330160f8d600
JJangseong <sungjin5891@gmail.com>
2020-04-30 17:54:32 +0900

    REACT - API Clear

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
 
 const app = express();
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 const database = {
   users: [
@@ -24,6 +25,13 @@ const database = {
       joined: new Date(),
     },
   ],
+  login: [
+    {
+      id: "987",
+      has: "",
+      email: 'john@gmail.com"',
+    },
+  ],
 };
 
 app.get("/", (req, res) => {
@@ -31,6 +39,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
+  // res === false or true
+  bcrypt.compare("bacon", hash, (err, res) => {});
+  bcrypt.compare("veggies", hash, (err, res) => {});
+
   const { email, password } = req.body;
   if (
     email === database.users[0].email &&
@@ -44,7 +56,7 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  database.users.concat({
+  database.users.push({
     id: "125",
     name: name,
     email: email,
@@ -55,6 +67,41 @@ app.post("/register", (req, res) => {
 
   res.json(database.users[database.users.length - 1]);
 });
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      res.json(user);
+    }
+  });
+
+  if (!found) {
+    res.status(400).json(" no such user ");
+  }
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+
+  if (!found) {
+    res.status(400).json(" no such user ");
+  }
+});
+
+
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
